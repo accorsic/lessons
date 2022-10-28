6ca2311bd7cd05c07e76ae5dea708a64eed1114e
Cristina Maria Accorsi Ruttgers <Cristina.Maria.Accorsi.Ruttgers@es.ey.com>
2022-10-28 11:03:21 +0200

    viernes

--- unit_2/.ipynb_checkpoints/2.08LAB-checkpoint.sql
+++ unit_2/.ipynb_checkpoints/2.08LAB-checkpoint.sql
@@ -16,28 +16,20 @@ WHERE length IS NOT NULL;
 
 -- 3 How many films are there for each of the categories in the category table? Hint: Use appropriate join between the tables "category" and "film_category".
 -- left join
-select a.category_id, a.name, l.film_id, group by name, COUNT(film_id) AS number_of_films
-from sakila.category as a
-left join sakila.film_category as l on a.category_id = l.category_id
-order by a.category_id;
+select a.catedory_id, a.name, c.film_id, count(film_id) as number_of_films
+from sakila.category as c 
+left join sakila.category as l on a.category_id = l.category_id
+group by a.category_id;
 
 -- 4 Which actor has appeared in the most films? Hint: You can create a join between the tables "actor" and "film actor" and count the number of times an actor appears.alter
 
-select a.actor_id, a.first_name, a.last_name, COUNT(film_id) AS number_of_times
-from sakila.actor as a
-left join sakila.film_actor as l on a.actor_id = l.actor_id
+select a.actor, l.film_id, a.first_name, count(film_id) as number_of_films
+from sakila.category as a
+left join sakila.film_category as l on a.category_id = l.category_id
 group by a.actor_id;
 
 -- 5 Which is the most active customer (the customer that has rented the most number of films)? Hint: Use appropriate join between the tables "customer" and "rental" and count the rental_id for each customer.
 
-select a.rental_id, l.customer_id, l.first_name, l.last_name, l.active, COUNT(rental_id) AS number_of_times
-from sakila.rental as a
-right join sakila.customer as l on a.customer_id = l.customer_id
-WHERE l.active = 1
-group by customer_id
-order by number_of_times desc;
-
--- bonus
-select film_id, title
-from sakila.film
-where film_id = 103;
+select active, customer_id, rental_id as customer_activity
+from sakila.customer as c
+order by active;
